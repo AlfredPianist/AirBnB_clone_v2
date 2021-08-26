78e17aeb7c788c2b8a524053d95da322cb2c1c98
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-26 06:27:06 +0000

    Fix: delete pack for 0x03

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,29 +5,6 @@ from fabric.api import local, hosts, put, run, env
 env.hosts = ['34.75.153.110', '18.208.193.84']
 
 
-def do_pack():
-    """Packs contents of web_static as a .tgz and returns its filepath."""
-    from os import mkdir, path
-    from datetime import datetime
-
-    now = datetime.now()
-    filename = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-    filepath = "versions/{}".format(filename)
-
-    try:
-        mkdir('./versions')
-    except FileExistsError:
-        pass
-
-    print("Packing web_static to {}".format(filepath))
-    cmd = local('tar -cvzf {} web_static'.format(filepath))
-    if (cmd.return_code == 0):
-        filesize = path.getsize(filepath)
-        print("web_static packed: {} -> {}Bytes".format(filepath, filesize))
-        return filepath
-    return None
-
-
 def do_deploy(archive_path):
     """Distributes an archive to the web servers"""
     from os import path
