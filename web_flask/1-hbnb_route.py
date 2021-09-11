1d753fbecc356956fcace5d8b6deb5791455865c
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-10 20:21:01 -0500

    Add: task 1 and added documentation for 0

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,5 +12,11 @@ def index():
     return "Hello, HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_index():
+    """Returns 'HBNB' on route /hbnb"""
+    return "HBNB"
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
