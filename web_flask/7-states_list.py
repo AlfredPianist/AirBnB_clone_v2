ca71952f89c7bf72a3d59d7b9a4daa3a6e0fcc21
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-09-12 14:32:25 -0500

    Add: task 7 part 2

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(self):
+    """Closes connection with db"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Returns a list of tuples of states with its id and name"""
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000", debug=True)
